4e9241877ef340beb8037cd218e965ffb9553e09
Adrien Tiburce <adrien.tiburce@gmail.com>
2020-12-02 09:00:09 +0100

    Feat: day 2 :zap:

--- 2020/day_1/solution.py
+++ 2020/day_1/solution.py
@@ -1,29 +1,31 @@
-input = open('input.txt', 'r') 
-lines = input.readlines() 
-  
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+input = open(f'{dir_path}/input.txt', 'r')
+lines = input.readlines()
+
 count = 0
 numbers = []
-for line in lines: 
-	try:
-		numbers.append(int(line.rstrip()))
-	except:
-		print("not a number")
-	count += 1
+for line in lines:
+    try:
+        numbers.append(int(line.rstrip()))
+    except:
+        print("not a number")
+    count += 1
 
 for i in range(len(numbers)):
-	for j in range(i+1, len(numbers)):
-		n1, n2 = numbers[i], numbers[j]
-		if n1 + n2 == 2020:
-			print(f" {n1} + {n2} = 2020 >> {n1*n2}")
-			i = len(numbers)
-			break
-			
+    for j in range(i+1, len(numbers)):
+        n1, n2 = numbers[i], numbers[j]
+        if n1 + n2 == 2020:
+            print(f" {n1} + {n2} = 2020 >> {n1*n2}")
+            i = len(numbers)
+            break
+
 
 for i in range(len(numbers)):
-	for j in range(i+1, len(numbers)):
-		for k in range(j+1, len(numbers)):
-			n1, n2, n3 = numbers[i], numbers[j], numbers[k]
-			if n1 + n2 + n3 == 2020:
-				print(f" {n1} + {n2} + {n3} = 2020 >> {n1*n2*n3}")
-				exit()
-
+    for j in range(i+1, len(numbers)):
+        for k in range(j+1, len(numbers)):
+            n1, n2, n3 = numbers[i], numbers[j], numbers[k]
+            if n1 + n2 + n3 == 2020:
+                print(f" {n1} + {n2} + {n3} = 2020 >> {n1*n2*n3}")
+                exit()
---
+++ 2020/day_2/solution.py
@@ -0,0 +1,24 @@
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+input = open(f'{dir_path}/input.txt', 'r')
+lines = input.readlines()
+
+# ======= PART ONE ========
+res = 0
+res2 = 0
+for line in lines:
+    ll = line.split(" ")
+    n1, n2 = map(int, ll[0].split("-"))
+    letter = ll[1][:1]
+    password = ll[2]
+    if n1 <= password.count(letter) <= n2:
+        res += 1
+# ======= PART TWO ========
+    if password[n1-1] == letter and not(password[n2-1] == letter):
+        res2 += 1
+    elif password[n2-1] == letter and not(password[n1-1] == letter):
+        res2 += 1
+
+print(f"Part 1: valid passord : {res}/{len(lines)}")
+print(f"Part 2: valid passord : {res2}/{len(lines)}")
