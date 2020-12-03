0c20094c9fca882b6458f521f1bc19453068e39d
Adrien Tiburce <adrien.tiburce@gmail.com>
2020-12-03 09:05:43 +0100

    Add: day 3 😎

---
+++ 2020/day_3/solution.py
@@ -0,0 +1,34 @@
+import os
+import numpy
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+input = open(f'{dir_path}/input.txt', 'r')
+lines = input.readlines()
+
+n = len(lines)
+lenLine = len(lines[0].rstrip())
+
+
+def getSlopeTrees(xSlope -> int, ySlope -> int) -> int:
+    count = 0
+    xPos = 0
+    for yPos in range(ySlope, n, ySlope):
+        xPos += xSlope
+        char = lines[yPos][xPos % lenLine]
+        count += (char == "#")
+    return count
+
+
+# PART ONE:
+print(getSlopeTrees(1, 2))
+
+# PART TWO:
+xSlopes = [1, 3, 5, 7, 1]
+ySlopes = [1, 1, 1, 1, 2]
+
+allRes = []
+for i in range(len(xSlopes)):
+    res = getSlopeTrees(xSlopes[i], ySlopes[i])
+    allRes.append(res)
+print(allRes)
+print(numpy.prod(allRes))
